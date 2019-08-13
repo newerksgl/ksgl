3ea3e94d726798f01c395f1478ea856e5ca7a76a
yunluo-xingchen <yunluo-xingchen@users.noreply.github.com>
2019-08-13 09:43:35 +0800

    谢涛
    修改了test文件

--- ksgl/ksgl/src/test/java/com/newer/ksgl/model/ModelApplicationTests.java
+++ ksgl/ksgl/src/test/java/com/newer/ksgl/model/ModelApplicationTests.java
@@ -15,7 +15,6 @@ public class ModelApplicationTests {
     @Test
     public void contextLoads() {
         testUsersService.test_findAll();
-        System.out.println(1);
         System.out.println(2);
     }
 
